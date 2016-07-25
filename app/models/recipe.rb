88648cbc2796d5480d0528b883754236f4a46c3d
Sean Handley <sean.handley@gmail.com>
2016-07-25 08:31:08 +0100

    Calculate costs.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,7 @@
 class Recipe < ApplicationRecord
   has_many :recipe_ingredients, inverse_of: :recipe
+
+  def total_pence
+    recipe_ingredients.collect(&:pence).sum
+  end
 end
--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -2,4 +2,8 @@ class RecipeIngredient < ApplicationRecord
   belongs_to :recipe,              inverse_of: :recipe_ingredients
   belongs_to :supplier_ingredient, inverse_of: :recipe_ingredients
   belongs_to :unit,                inverse_of: :recipe_ingredients
+
+  def pence
+    cost.pence_per_whole_unit * quantity
+  end
 end

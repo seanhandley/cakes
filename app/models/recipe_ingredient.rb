b61172733c4d02636cbdc9fe642119264d692c58
Sean Handley <sean.handley@gmail.com>
2016-08-01 15:02:33 +0100

    Ensure the units match.

--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -3,7 +3,17 @@ class RecipeIngredient < ApplicationRecord
   belongs_to :supplier_ingredient, inverse_of: :recipe_ingredients
   belongs_to :unit,                inverse_of: :recipe_ingredients
 
+  validate :unit_types_match
+
   def pence
     supplier_ingredient.pence_per_whole_unit * quantity
+  end
+
+  private
+
+  def unit_types_match
+    if unit_id != supplier_ingredient.unit_id
+      @errors.add(:unit, "doesn't match supplier ingredient unit (#{supplier_ingredient.unit.name})")
+    end
   end
 end
--- test/models/recipe_ingredient_test.rb
+++ test/models/recipe_ingredient_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class TestRecipeIngredient < CleanTest
   def setup
     gram = Unit.make!(:gram)
+    @cup = Unit.make!(:cup)
     args = {
       quantity: 100,
       unit: gram,
@@ -19,5 +20,16 @@ class TestRecipeIngredient < CleanTest
 
   def test_pence
     assert_equal 1000, @recipe_ingredient.pence
+  end
+
+  def test_unit_matches
+    args = {
+      supplier_ingredient: @supplier_ingredient,
+      unit: @cup,
+      quantity: 500
+    }
+    assert_raises ActiveRecord::RecordInvalid do
+      @recipe_ingredient = RecipeIngredient.make! args
+    end
   end
 end
